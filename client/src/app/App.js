7f2882b0364e5657b54d87b404524706e38e0564
Chloe Miranz <chloemiranz@gmail.com>
2020-07-29 09:04:25 -0700

    adding App.js

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
- 
+
 import './App.css';
- 
-//Components
-import Navbar from './components/navbar';
- 
+
+// Components
+import Navbar from './components/Navbar';
+
 // Pages
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 import Team from './pages/Team';
 import SignIn from './pages/SignIn';
- 
- 
-const App = () => (
- <Router>
-   <Navbar />
-   <Route exact path= "/" component={Home} />
-   <Route exact path= "/explore" component={Explore} />
-   <Route exact path= "/team" component={Team} />
-   <Route exact path= "/signin" component={SignIn} />
- </Router>
-);
- 
-export default App;
- 
+import SignUp from './pages/SignUp';
 
+const App = () => (
+  <Router>
+    <Navbar />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/explore" component={Explore} />
+    <Route exact path="/team" component={Team} />
+    <Route exact path="/signin" component={SignIn} />
+    <Route exact path="/signup" component={SignUp} />
+  </Router>
+);
+
+export default App;
