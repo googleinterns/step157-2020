8a6e2f31a693daa8c6dba166c376724758fdea4a
Chibuzo Chiwike <chibuzo.m.chiwike.22@dartmouth.edu>
2020-08-04 20:42:30 -0400

    Revert file to original

--- client/src/api/user-api.js
+++ client/src/api/user-api.js
@@ -1,13 +1,12 @@
 import firebase from '../firebase.js';
 
-const storeNewUserInDatabase = (id) => {
+const storeUserInDatabase = (id) => {
   const user = {
-    name: '',
-    age: '',
-    bio: '',
-    photoUrl: '',
-    skillsToTeach: [''],
-    skillsToLearn: [''],
+    name: 'Jo Bamba',
+    age: 22,
+    bio: 'Click edit to edit your bio!',
+    skillsToTeach: ['sports'],
+    skillsToLearn: ['cooking'],
   };
   firebase.database().ref('users').child(id).set(user);
 };
@@ -17,9 +16,9 @@ export const createUser = (email, password) => {
     .createUserWithEmailAndPassword(email, password)
     .then(() => firebase.auth().currentUser.uid)
     .then((uid) => {
-      storeNewUserInDatabase(uid);
+      storeUserInDatabase(uid);
     })
-    .catch(() => {});
+    .catch((ignore) => {});
 };
 
 export const signInUser = (email, password) => {
@@ -27,25 +26,9 @@ export const signInUser = (email, password) => {
     .signInWithEmailAndPassword(email, password)
     .then(() => firebase.auth().currentUser.uid)
     .then((uid) => uid)
-    .catch(() => {});
+    .catch((ignore) => {});
 };
 
 export const signOutUser = () => {
-  firebase.auth().signOut().then(() => {}).catch(() => {});
-};
-
-export const fetchUser = async (id) => {
-  const snapshot = await firebase.database().ref('users').child(id).once('value');
-  return snapshot.val();
-};
-
-export const updateUser = (id, userProfile) => {
-  firebase.database().ref('users').child(id).update(userProfile);
-};
-
-export const uploadProfilePicture = async (id, file) => {
-  const profileReference = firebase.storage().ref('images').child(`${id}/profile`);
-  await profileReference.put(file);
-  const url = await profileReference.getDownloadURL();
-  return url;
+  firebase.auth().signOut().then(() => {}).catch((ignore) => {});
 };
