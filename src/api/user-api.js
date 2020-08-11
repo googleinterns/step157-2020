79426fc91ffea17958d1297a705dfdea26cdeea6
Chloe Miranz <chloemiranz@gmail.com>
2020-08-11 11:42:30 -0700

    all changes

---
+++ src/api/user-api.js
@@ -0,0 +1,34 @@
+import firebase from '../firebase.js';
+
+const storeUserInDatabase = (id) => {
+  const user = {
+    name: 'Jo Bamba',
+    age: 22,
+    bio: 'Click edit to edit your bio!',
+    skillsToTeach: ['sports'],
+    skillsToLearn: ['cooking'],
+  };
+  firebase.database().ref('users').child(id).set(user);
+};
+
+export const createUser = (email, password) => {
+  firebase.auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => firebase.auth().currentUser.uid)
+    .then((uid) => {
+      storeUserInDatabase(uid);
+    })
+    .catch((ignore) => {});
+};
+
+export const signInUser = (email, password) => {
+  firebase.auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(() => firebase.auth().currentUser.uid)
+    .then((uid) => uid)
+    .catch((ignore) => {});
+};
+
+export const signOutUser = () => {
+  firebase.auth().signOut().then(() => {}).catch((ignore) => {});
+};
